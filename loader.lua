d034ae25d672d67e7aa2400fdd913a6c4f98093d
RVZ <revyymail@gmail.com>
2023-03-05 18:51:25 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -37,8 +37,8 @@ pcall(function()
 end)
 
 --------------Framework & Library-----------------
-loadstring(game:HttpGet('https://raw.githubusercontent.com/RedyRed/North/main/elements/framework'))()
-Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/RedyRed/Rayfield-Custom/main/source'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/RedyRed/North/main/elements/framework.lua'))()
+Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/RedyRed/Rayfield-Custom/main/source.lua'))()
 ----------------------------------------
 
 if Debug then return end
@@ -53,25 +53,20 @@ end) then
 
     local diff = (current-updSec)/1000/60/60
     if diff <= 24 then
-        Rayfield:Notify({
-            Title = "North Hub",
-            Content = 'WARNING: The game has been updated '..math.floor(diff)..' hours ago, which means that some functions may be patched by now. Are you sure you want to proceed?',
+        local bindable = Instance.new("BindableFunction")
+        function bindable.OnInvoke(response)
+            if response == "Yes" then
+                loadstring(game:HttpGet(url))()
+            end
+        end
+        game.StarterGui:SetCore("SendNotification",{
+            Title = 'North Hub',
+            Text = 'WARNING: The game has been updated '..diff..' hours ago. Proceed?',
             Duration = 300,
-            Image = 2804603863,
-            Actions = {
-                x = {
-                    Name = 'Yes',
-                    Callback = function() 
-                        loadstring(game:HttpGet(url))()
-                    end
-                },
-                y = {
-                    Name = 'No',
-                    Callback = function()
-                        return
-                    end
-                }
-             }
+            Icon = 'rbxassetid://12660955323',
+            Callback = bindable,
+            Button1 = 'Yes',
+            Button2 = 'No'
         })
     else
         loadstring(game:HttpGet(url))()
